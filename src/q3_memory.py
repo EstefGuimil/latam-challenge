33e06fb69e566754776232341468655df9167b96
EstefiGuimil <estefania.guimil@mercadolibre.com>
2023-11-17 23:47:53 -0300

    Sumo archivo de testing y cambio algunos detalles en las funciones que surgieron del testing

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -6,19 +6,25 @@ import auxiliar_functions as aux_fun
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
     # Leo el archivo json que se encuentra en la ruta compartida
     df_tweets = aux_fun.read_json_file_spark(file_path)
-    # Me quedo solo con la informacion relevante: los username mencionados en los tweets (username dentro del nodo mentionedUsers)
-    df_users = df_tweets.select(df_tweets.mentionedUsers.username.alias("mentioned_users"))
-    # Filtro las filas donde el arreglo de usuarios mecionados esta vacio
-    df_users_filter = df_users.filter(size(df_users.mentioned_users) > 0)
-    # Con la función explode convierto cada usuario dentro de la lista de mencionados en una nueva fila del data frame
-    explode_df = df_users_filter.select("*", explode("mentioned_users").alias("mentioned_user"))
-    # Elimino la columna que tiene el arreglo de mencionados que ya no es necesaria
-    explode_df = explode_df.drop(explode_df.mentioned_users)
-    # Cuento por usuario mencionado la cantidad de menciones que tuvo en toda la historia
-    counts = explode_df.groupBy("mentioned_user").count()
-    # Ordeno en forma descendente por cantidad de menciones y me quedo con los diez primeros para tener el top 10
-    top_ten_mentioned_user = counts.orderBy(col("count").desc()).limit(10)
-    # Convierto el dataframe en una lista de tuplas (mentioned_user, count)
-    result_list = top_ten_mentioned_user.rdd.map(lambda x: (x[0], x[1])).collect()
-    # Retorno la lista resultante del paso anterior
-    return(result_list)
+    if (df_tweets.count() == 0):
+        return []
+    else:
+        try:
+            # Me quedo solo con la informacion relevante: los username mencionados en los tweets (username dentro del nodo mentionedUsers)
+            df_users = df_tweets.select(df_tweets.mentionedUsers.username.alias("mentioned_users"))
+            # Filtro las filas donde el arreglo de usuarios mecionados esta vacio
+            df_users_filter = df_users.filter(size(df_users.mentioned_users) > 0)
+            # Con la función explode convierto cada usuario dentro de la lista de mencionados en una nueva fila del data frame
+            explode_df = df_users_filter.select("*", explode("mentioned_users").alias("mentioned_user"))
+            # Elimino la columna que tiene el arreglo de mencionados que ya no es necesaria
+            explode_df = explode_df.drop(explode_df.mentioned_users)
+            # Cuento por usuario mencionado la cantidad de menciones que tuvo en toda la historia
+            counts = explode_df.groupBy("mentioned_user").count()
+            # Ordeno en forma descendente por cantidad de menciones y me quedo con los diez primeros para tener el top 10
+            top_ten_mentioned_user = counts.orderBy(col("count").desc()).limit(10)
+            # Convierto el dataframe en una lista de tuplas (mentioned_user, count)
+            result_list = top_ten_mentioned_user.rdd.map(lambda x: (x[0], x[1])).collect()
+            # Retorno la lista resultante del paso anterior
+            return(result_list)
+        except AttributeError:
+            return []  # Casos en que no existe en el archivo que leí ningun registro con mentionedUsers
--- src/q3_time.py
+++ src/q3_time.py
@@ -9,19 +9,22 @@ def get_mentioned_users(mencioned_list):
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
     # Creo un dataframe con todos los jsons
     df = aux_fun.read_json_file_pandas(file_path)
-    # Elimino todas las filas que no mencionan usuarios
-    df = df[df['mentionedUsers'].notnull()]
-    # Sumo la columna mentioned_users comn las lista de usernames mencionados
-    df['mentioned_users'] = df['mentionedUsers'].apply(lambda l: get_mentioned_users(l))
-    # Me quedo solo con la columna que me importa, la que contiene la lista los usuarios mencionados en el tweet
-    df = df[['mentioned_users']]
-    # Con la función explode convierto cada usuario dentro de la lista de mencionados en una nueva fila del data frame
-    explode_df = df.explode("mentioned_users")
-    # Cuento por usuario mencionado la cantidad de menciones que tuvo en toda la historia
-    counts = explode_df.groupby(['mentioned_users']).size().reset_index(name='count')
-    # Ordeno en forma descendente por cantidad de menciones y me quedo con los diez primeros para tener el top 10
-    result = counts.sort_values(by=['count'], ascending=False).head(10)
-    # Convierto el dataframe en una lista de tuplas (mentioned_user, count)
-    result_list = list(zip(*map(result.get, ('mentioned_users', 'count'))))
-    # Retorno la lista resultante del paso anterior
-    return result_list
+    if df.empty:
+        return []
+    else:
+        # Elimino todas las filas que no mencionan usuarios
+        df = df[df['mentionedUsers'].notnull()]
+        # Sumo la columna mentioned_users comn las lista de usernames mencionados
+        df['mentioned_users'] = df['mentionedUsers'].apply(lambda l: get_mentioned_users(l))
+        # Me quedo solo con la columna que me importa, la que contiene la lista los usuarios mencionados en el tweet
+        df = df[['mentioned_users']]
+        # Con la función explode convierto cada usuario dentro de la lista de mencionados en una nueva fila del data frame
+        explode_df = df.explode("mentioned_users")
+        # Cuento por usuario mencionado la cantidad de menciones que tuvo en toda la historia
+        counts = explode_df.groupby(['mentioned_users']).size().reset_index(name='count')
+        # Ordeno en forma descendente por cantidad de menciones y me quedo con los diez primeros para tener el top 10
+        result = counts.sort_values(by=['count'], ascending=False).head(10)
+        # Convierto el dataframe en una lista de tuplas (mentioned_user, count)
+        result_list = list(zip(*map(result.get, ('mentioned_users', 'count'))))
+        # Retorno la lista resultante del paso anterior
+        return result_list
